39324dfc1999e27421aea35c525da8ba40cc699b
Teodor Sakal Franciskovic <sakal.teodor@gmail.com>
2022-04-15 23:46:07 +0200

    16.2.4. Napraviti stranicu profila za instruktora pecanja koristeći Vue.js

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,12 +1,15 @@
 const RegistrationRequests = { template: '<registration-requests></registration-requests>'};
 const RegistrationRequestsRow = {template: '<registration-requests-row> </registration-requests-row>'};
+const NavFishingInstructor = {template: '<nav-fishing-instructor> </nav-fishing-instructor>'};
+const ProfilePageFishingInstructor = {template: '<profile-page-fishing-instructor> </profile-page-fishing-instructor>'}
 
 const router = new VueRouter({
     mode: 'hash',
     routes: [
         {path: "/registration-requests", component: RegistrationRequests},
-        {path: "/registration-requests-row", component: RegistrationRequestsRow}
-
+        {path: "/registration-requests-row", component: RegistrationRequestsRow},
+        {path: "/profile-page-fishing-instructor", component: ProfilePageFishingInstructor},
+        {path: "/nav-fishing-instructor", component: NavFishingInstructor},
     ]
 });
 
