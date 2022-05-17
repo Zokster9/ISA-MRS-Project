6d794f934b280f4340a8b123bd0bacb42f92d974
Zoran Bukorac <zoran00.zb@gmail.com>
2022-05-17 17:53:22 +0200

    10.4 Slati mejl na definisanje nove akcije na koju je klijent pretplaćen

--- SpringProject/src/main/java/com/example/projectmrsisa/controller/ShipController.java
+++ SpringProject/src/main/java/com/example/projectmrsisa/controller/ShipController.java
@@ -144,7 +144,7 @@ public class ShipController {
     }
 
     @GetMapping(value = "/get/{id}", produces = "application/json")
-    @PreAuthorize("hasAnyRole('shipOwner', 'admin', 'mainAdmin')")
+    @PreAuthorize("hasAnyRole('shipOwner', 'admin', 'mainAdmin', 'client')")
     public ResponseEntity<ShipDTO> getShipById(@PathVariable Integer id) {
         try {
             Ship ship = shipService.findShipById(id);
